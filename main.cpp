4358ea85b1a86b3be1ba6693a9c1100a24ef83eb
carlosvts <carlosvtsdev@gmail.com>
2026-03-28 14:22:09 -0300

    hello raylib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <raylib.h>
 
+constexpr int WIDTH = 800;
+constexpr int HEIGHT = 600;
+
 int main()
 {
-	std::cout << "Hello world!\n Raylib plotter!";
+	InitWindow(WIDTH, HEIGHT, "function plotter - carlosvts");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			ClearBackground(BLACK);
+			DrawText("Plotter", 190, 200, 20, RAYWHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
 }
